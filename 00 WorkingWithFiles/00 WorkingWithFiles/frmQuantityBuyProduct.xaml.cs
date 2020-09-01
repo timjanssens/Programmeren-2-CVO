30070a8b25ac34cd538543469740ea090a865980
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-09-01 20:36:47 +0200

    Add Layout forms

---
+++ 00 WorkingWithFiles/00 WorkingWithFiles/frmQuantityBuyProduct.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _00_WorkingWithFiles
+{
+    /// <summary>
+    /// Interaction logic for frmQuantityBuyProduct.xaml
+    /// </summary>
+    public partial class frmQuantityBuyProduct : Window
+    {
+        public frmQuantityBuyProduct()
+        {
+            InitializeComponent();
+        }
+    }
+}
